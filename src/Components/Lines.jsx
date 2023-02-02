779553710460abdce944aece4736e0dbb59506cd
Joszko <jakub.rezler96@gmail.com>
2023-02-02 21:43:43 +0100

    create Lines component

---
+++ src/Components/Lines.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../style/css/Lines.css";
+
+const Lines = () => {
+	return (
+		<div className="lines">
+			<div className="vertical-line"></div>
+			<div className="vertical-line"></div>
+			<div className="vertical-line"></div>
+			<div className="vertical-line"></div>
+			<div className="vertical-line"></div>
+		</div>
+	);
+};
+
+export default Lines;
