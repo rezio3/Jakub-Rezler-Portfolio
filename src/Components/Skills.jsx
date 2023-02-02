68fff63b69ade00cb6bb721ce697879c6b1e18e2
Joszko <jakub.rezler96@gmail.com>
2023-02-03 00:57:54 +0100

    add images to span containers

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,5 +1,15 @@
 import React, { useRef, useEffect, useState } from "react";
 import "../style/css/Skills.css";
+import "../style/css/SkillsAnim.css";
+import htmlImg from "../img/html.png";
+import cssImg from "../img/css.png";
+import jsImg from "../img/js.png";
+import reactImg from "../img/react.png";
+import nodeImg from "../img/node.png";
+import expressImg from "../img/express.png";
+import mongoImg from "../img/mongo.png";
+import gimpImg from "../img/gimp.png";
+import figmaImg from "../img/figma.png";
 
 const Skills = () => {
 	const [anim, setAnim] = useState(false);
@@ -19,13 +29,33 @@ const Skills = () => {
 				</span>
 			</div>
 			<div className="technologies-container">
-				<span className="html-text skill">HTML5</span>
-				<span className="css-text skill">CSS3/SCSS/Bootstrap</span>
-				<span className="js-text skill">Javascript ES6</span>
-				<span className="react-text skill">React.js</span>
-				<span className="node-text skill">Node.js</span>
-				<span className="express-text skill">Express.js</span>
-				<span className="mongodb-text skill">MongoDB</span>
+				<span className="html-text skill">
+					<img src={htmlImg} className="html-css-js-img" />
+				</span>
+				<span className="css-text skill">
+					<img src={cssImg} className="html-css-js-img" />
+				</span>
+				<span className="js-text skill">
+					<img src={jsImg} className="html-css-js-img" />
+				</span>
+				<span className="react-text skill">
+					<img src={reactImg} className="react-img" />
+				</span>
+				<span className="node-text skill">
+					<img src={nodeImg} className="node-express-mongo-img" />
+				</span>
+				<span className="express-text skill">
+					<img src={expressImg} className="node-express-mongo-img" />
+				</span>
+				<span className="mongodb-text skill">
+					<img src={mongoImg} className="node-express-mongo-img" />
+				</span>
+				<span className="gimp-text skill">
+					<img src={gimpImg} className="gimp-figma-img" />
+				</span>
+				<span className="figma-text skill">
+					<img src={figmaImg} className="gimp-figma-img" />
+				</span>
 			</div>
 		</div>
 	);
