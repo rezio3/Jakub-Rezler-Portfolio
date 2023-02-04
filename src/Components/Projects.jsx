ed6ff6ac980ab100d526f926fb94a1416c71a199
Joszko <jakub.rezler96@gmail.com>
2023-02-04 17:57:17 +0100

    add Projects.jsx component with first project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "../style/css/Projects.css";
 
-const Projects = () => {
+const Projects = (props) => {
+	const myRef = useRef();
+	useEffect(() => {
+		const observer = new IntersectionObserver((entries) => {
+			const entry = entries[0];
+			console.log(entry.target.id);
+			props.setBgColor({
+				...props.bgColor,
+				[entry.target.id]: entry.isIntersecting,
+			});
+		});
+		observer.observe(myRef.current);
+	}, []);
+
+	// console.log(bgColor);
 	return (
-		<div>
+		<div className="projects-container">
 			<h2 className="projects-header">Projekty</h2>
+			<div ref={myRef} className="stay-alive-project-container" id="stayAlive">
+				<span className="stay-alive-upper-text stay-alive-text">
+					Stay Alive Boardgame
+				</span>
+				<span className="stay-alive-lower-text stay-alive-text">
+					JS/CSS/HTML
+				</span>
+			</div>
 		</div>
 	);
 };
