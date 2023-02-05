20973662ef5167ba0a63d0389e0a0c53941e2fa1
Joszko <jakub.rezler96@gmail.com>
2023-02-05 02:29:45 +0100

    create footer and add icons

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../style/css/Footer.css";
+
+const Footer = () => {
+	return (
+		<div className="footer-container">
+			<span className="copyright">Jakub Rezler &copy; 2023</span>
+			<div className="footer-links-container">
+				<i class="fa-brands fa-github"></i>=
+				<i class="fa-brands fa-linkedin"></i>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
